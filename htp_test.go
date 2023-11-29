f7dd6e9147d71571c1fc71bb3c750a9e9ef377b7
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-11-29 10:15:49 +0700

    test http

---
+++ htp_test.go
@@ -0,0 +1,29 @@
+package belajar_golang_web_eko
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func HelloHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Hello Isekai")
+}
+
+func TestHttp(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+
+	response := recorder.Result()
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	bodyString := string(body)
+	fmt.Println(bodyString)
+}
