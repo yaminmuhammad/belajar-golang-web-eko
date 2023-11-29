998e259bf6b4af5933a7ccd0c70a95d8ae905c75
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-11-29 10:02:18 +0700

    test request

--- handler_test.go
+++ handler_test.go
@@ -43,3 +43,20 @@ func TestServeMux(t *testing.T) {
 		log.Fatal(err)
 	}
 }
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, request.Method)
+		fmt.Fprint(writer, request.RequestURI)
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
