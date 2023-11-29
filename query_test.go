0b4f38737eb4d184637cda130f6cddc00e7c6de7
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-11-29 20:37:42 +0700

    test multiple query param

--- query_test.go
+++ query_test.go
@@ -29,3 +29,22 @@ func TestQueryParameter(t *testing.T) {
 
 	fmt.Println(string(body))
 }
+
+func MultipleQueryParameter(writer http.ResponseWriter, request *http.Request) {
+	firtname := request.URL.Query().Get("first_name")
+	lastname := request.URL.Query().Get("last_name")
+
+	fmt.Fprintf(writer, "Hello %s %s", firtname, lastname)
+}
+
+func TestMultipleQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?first_name=Ekologi&last_name=Jancok", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleQueryParameter(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
