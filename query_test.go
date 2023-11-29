94f00aecb4de96fcaf571065c7c962b88148508e
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-11-29 20:26:02 +0700

    test query param

---
+++ query_test.go
@@ -0,0 +1,31 @@
+package belajar_golang_web_eko
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SayHello(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+
+	if name == "" {
+		fmt.Fprint(writer, "Hello")
+	} else {
+		fmt.Fprintf(writer, "Helo, %s", name)
+	}
+}
+
+func TestQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?name=Ekologi", nil)
+	recorder := httptest.NewRecorder()
+
+	SayHello(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
